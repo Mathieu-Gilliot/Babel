613f26c224e77ac8262db25f5d3fe3de23ac4ce6
Mathieu Gilliot <mathieu.gilliot.pro@gmail.com>
2020-07-22 00:16:53 +0200

    test js

--- babel.js
+++ babel.js
@@ -1,16 +1,31 @@
-const btn = document.querySelector('#name');
+// const btn = document.querySelector('#name');
 
 
 
-btn.addEventListener('click',show);
+// btn.addEventListener('click',show);
 
-function show(){
-    let caroussel= document.querySelector(".exemple");
-    caroussel.classList.toggle('show')
-}
+// function show(){
+//     let caroussel= document.querySelector(".exemple");
+//     caroussel.classList.toggle('show')
+// }
 
-// Work in progress
+// Work in progress généralisation des boutons
 
-let listlink = document.querySelectorAll("[data-encart]")
+let listEncarts = document.querySelectorAll('[data-encart]');
+let listButtons = document.querySelectorAll('button');
 
-let listbuttons = document.querySelectorAll("[data-button]")
+console.log(listButtons);
+
+for (let item of listEncarts) {
+    let attribut = item.getAttribute("data-encart");
+    for (let bou of listButtons) {
+        let id = bou.getAttribute("id");
+    }
+};
+
+
+
+
+
+
+
